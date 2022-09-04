1f93d86df8daf2c76a84cf79e96e357f2e17b038
Alexander Odoom <aaodoom@gmail.com>
2022-09-04 05:14:39 +0000

    add startup message to payment service

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,7 +6,7 @@ import { natsWrapper } from "./nats-wrapper";
 
 
 const start = async () => {
-
+    console.log('App is starting...')
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined')
     }
