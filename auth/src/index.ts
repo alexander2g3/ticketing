f40fd716e210dead77473beac6ed827080652be9
Alexander Odoom <aaodoom@gmail.com>
2022-09-04 11:25:29 +0000

    Updated startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 
 
 const start = async () => {
-    console.log('Authentication service is starting up...')
+    console.log('Auth service is starting up...')
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined')
     }
